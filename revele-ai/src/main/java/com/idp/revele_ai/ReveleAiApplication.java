398d9e385f9360045a59469a2e99647ed17b5ce5
Elisandra Lima <elisandra.lima@souunit.com.br>
2025-09-28 13:45:34 -0300

    primeiro commit da api revele-ai

---
+++ revele-ai/src/main/java/com/idp/revele_ai/ReveleAiApplication.java
@@ -0,0 +1,13 @@
+package com.idp.revele_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReveleAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReveleAiApplication.class, args);
+	}
+
+}
---
+++ revele-ai/src/test/java/com/idp/revele_ai/ReveleAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idp.revele_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReveleAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
